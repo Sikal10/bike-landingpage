18bd8e3dcc00dfaf93cafd727cdcc27364ece758
Sikal <sikal.sikal.ss@gmail.com>
2021-11-03 17:55:06 +0100

    header styling fixed

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,10 +6,10 @@ const Header = ({isOpen, toggleIcon}) => {
     return (
         <main className={"md:relative sticky top-0 bg-gray-50 z-10"}>
             {/*desktop*/}
-            <header>
+            <header className={"hidden md:block"}>
 
                 {/*laptop navigation*/}
-                <nav className={"hidden md:flex flex items-center max-w-[907px] mx-auto pt-[56px] justify-between"}>
+                <nav className={"hidden md:flex flex items-center max-w-[907px] mx-auto pt-[40px] justify-between"}>
                     <img src="/images/logo.svg" className={"mt-[6px]"} alt=""/>
 
                     <div className={"flex space-x-7 mt-[14px] items-center"}>
@@ -28,8 +28,6 @@ const Header = ({isOpen, toggleIcon}) => {
                 <nav className={"md:hidden p-3 flex items-center justify-between"}>
                     <img src="/images/logo.svg" className={"mt-[6px]"} alt=""/>
 
-
-
                     {/*mobile-nav*/}
                     <div onClick={toggleIcon} className={"md:hidden text-3xl text-gray-400"}>{!isOpen && <HiOutlineMenu className={"text-4xl"} /> }</div>
                 </nav>
@@ -38,7 +36,7 @@ const Header = ({isOpen, toggleIcon}) => {
                 <div onClick={toggleIcon} className={`md:hidden ${isOpen && "transition duration-300 ease-in h-[100vh] fixed overflow-hidden w-2/6 top-0 right-0 left-0 bottom-0 bg-black opacity-50"}`}/>
 
                 {/*mobile-nav*/}
-                <aside className={`md:hidden ${isOpen ? "absolute -mt-3 top-0 right-0 h-screen fixed w-4/6 bg-white space-y-3" : "hidden"}`}>
+                <aside className={`md:hidden ${isOpen ? "absolute top-0 right-0 h-screen fixed w-4/6 bg-white space-y-3" : "hidden"}`}>
                     <p onClick={toggleIcon} className={"border-4 absolute right-[20px] top-[20px] font-light border-nosh_blue rounded-full border-opacity-30 w-[40px] h-[40px] px-[8px] pt-[8px]  text-center"}>
                         <FaTimes className={"text-md text-red-500"} />
                     </p>
